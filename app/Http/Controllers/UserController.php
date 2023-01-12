a4a35f4612f24ec04a939684a30c7e2cf35f1564
Favian <favouriteteo@gmail.com>
2023-01-12 17:41:54 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\UserDeletedJob;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware("auth:web")->except("login", "register", "restore");
+    }
+
+    public function login(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required|string",
+        ]);
+
+        if ($validation->fails()) {
+            return response([
+                "message" => "Invalid credentials",
+                "errors" => $validation->errors(),
+            ], 400);
+        }
+
+        if (!User::where('email', $request->email)->exists()) {
+            return response([
+                'message' => 'Email not found',
+            ], 404);
+        }
+
+        $token = Auth::attempt($request->all());
+
+        if (!$token) {
+            return response([
+                "message" => "Invalid credentials",
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        return response([
+            "message" => "Successfully logged in",
+            "user" => $user,
+            "token" => $token,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        if ($validation->fails()) {
+            return response([
+                "message" => "Invalid credentials",
+                "errors" => $validation->errors(),
+            ], 400);
+        }
+
+        if (User::where('email', $request->email)->exists()) {
+            return response([
+                'message' => 'Email already exists',
+            ], 409);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            "username" => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($user);
+
+        return response([
+            'message' => 'User created successfully',
+            'user' => $user,
+            "token" => $token,
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response([
+            "message" => "Logged out successfully",
+        ]);
+    }
+
+    public function me()
+    {
+        return response([
+            "user" => Auth::user(),
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'string|max:255',
+            "username" => "string|max:255",
+            'email' => 'string|email|max:255|unique:users',
+            'password' => 'string',
+        ]);
+
+        if ($validation->fails()) {
+            return response([
+                "message" => "Invalid credentials",
+                "errors" => $validation->errors(),
+            ], 400);
+        }
+
+        $user = Auth::user();
+        $user->update($request->all());
+
+        return response([
+            'status' => 'success',
+            'message' => 'User updated successfully',
+            'user' => $user,
+        ]);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $user = Auth::user();
+
+        if (Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'New password must be different from old one!',
+            ]);
+        }
+
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        Auth::logout();
+
+        return response([
+            "message" => "Password reset successfully!",
+        ]);
+    }
+
+    public function destroy(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            "password" => "required|string",
+        ]);
+
+        if ($validation->fails()) {
+            return response([
+                "message" => "Password required",
+                "errors" => $validation->errors(),
+            ], 400);
+        }
+
+        $user = Auth::user();
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        if (!$user) {
+            return response([
+                "message" => "Not logged in",
+            ], 401);
+        }
+
+        $user->delete();
+        UserDeletedJob::dispatch($user);
+        Auth::logout();
+
+        return response([
+            "message" => "User deleted successfully",
+        ]);
+    }
+
+    public function restore(Request $request) {
+        $validation = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required|string",
+        ]);
+
+        if ($validation->fails()) {
+            return response([
+                "message" => "Invalid credentials",
+                "errors" => $validation->errors(),
+            ], 400);
+        }
+
+        $trashedUser = User::withTrashed()->where("email", $request->email)->first();
+
+        if (!$trashedUser) {
+            return response([
+                'message' => 'Account associated with that email has not been deleted',
+            ], 404);
+        }
+
+        $trashedUser->restore();
+
+        $token = Auth::attempt($request->all());
+        $user = Auth::user();
+
+        return response([
+            "message" => "Successfully restored account",
+            "user" => $user,
+            "token" => $token,
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix("auth")->group(function () {
+    Route::post("login", [UserController::class, "login"]);
+    Route::post("register", [UserController::class, "register"]);
+    Route::post("logout", [UserController::class, "logout"]);
+    Route::get("me", [UserController::class, "me"]);
+    Route::post("refresh", [UserController::class, "refresh"]);
+    Route::delete("destroy", [UserController::class, "destroy"]);
+    Route::put("reset-password", [UserController::class, "resetPassword"]);
+    Route::put("restore", [UserController::class, "restore"]);
+});
