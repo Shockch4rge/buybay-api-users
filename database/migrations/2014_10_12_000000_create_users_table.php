95aaab20516ee3d9084c1d55a2d588f3b7b51c68
Favian <favouriteteo@gmail.com>
2023-02-06 22:37:00 +0800

    refactor: removed soft deletes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string("avatar_url")->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
