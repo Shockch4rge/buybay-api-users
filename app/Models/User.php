10f34a8993d8486cf1f9077ff126c085cc5d042e
Favian <favouriteteo@gmail.com>
2023-02-06 22:36:50 +0800

    refactor: removed soft deletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable, HasUuids, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids;
 
     /**
      * The attributes that are mass assignable.
